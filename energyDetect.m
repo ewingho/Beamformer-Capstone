6a0221739b279751290bce6ce757d65215f046f9
Ewing Ho <37677521+ewingho@users.noreply.github.com>
2018-04-04 10:34:39 -0400

    Add files via upload

--- energyDetect.m
+++ energyDetect.m
@@ -3,9 +3,16 @@ energyAngles = zeros(1,37);
 for i = 0:36
     energySum = 0;
     signalAtAngle = DAS(M, fs, y, c, d, 5*i);
-   for k = 1:length(y)
+    maxMag = 0;
+    for j = 1:length(y)
+        if(signalAtAngle(j) > maxMag)
+            maxMag = signalAtAngle(j);
+        end
+    end
+    signalAtAngle = signalAtAngle*maxMag;
+    for k = 1:length(y)
        energySum = energySum + abs(signalAtAngle(k))^2;
-   end
-   energyAngles(i+1) = energySum/time;
+    end
+    energyAngles(i+1) = energySum/time;
 end
 end
